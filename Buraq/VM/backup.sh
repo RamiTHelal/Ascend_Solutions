6df7bbee221f4016653275616806477d725fff2f
ramitalat <rami_helal@gmail.com>
2014-11-19 01:26:03 +0200

    Create backup.sh

---
+++ Buraq/VM/backup.sh
@@ -0,0 +1,48 @@
+############################################################
+# Backup-Script /var/lib/postgresql/backup.sh
+#
+# Reference: https://www.odoo.com/forum/help-1/question/how-to-setup-a-regular-postgresql-database-backup-4728
+############################################################
+ 
+#!/bin/bash
+
+# Location of the backup logfile.
+logfile="/var/backups/postgres_db/logfile.log"
+
+# Location to place backups.
+backup_dir="/var/backups/postgres_db"
+
+#String to append to the name of the backup files
+backup_date=`date +%Y-%m-%d_%H-%M`
+
+#Numbers of days you want to keep copie of your databases
+number_of_days=10
+touch $logfile
+databases=`psql -l -t | cut -d'|' -f1 | sed -e 's/ //g' -e '/^$/d'`
+for i in $databases; do
+  if [ "$i" != "template0" ] && [ "$i" != "template1" ] && [ "$i" != "postgres" ]; then
+    # backupfile=$backup_dir$i.$backup_date.sql.gz
+    # echo Dumping $i to $backupfile
+    # pg_dump $i|gzip > $backupfile
+	
+    timeinfo=`date '+%T %x'`
+    echo "Backup and Vacuum started at $timeinfo for time slot $timeslot on database: $i " >> $logfile
+    /usr/bin/vacuumdb -z -U postgres $i >/dev/null 2>&1
+    # /usr/bin/pg_dump $i -U postgres | gzip > "$backup_dir/openerp-$i-$timeslot-database.gz"
+    timeinfo=`date '+%T %x'`
+    pg_dump -U postgres --format=c --file=$backup_dir/$i-$timeslot.dump $i
+    gzip $backup_dir/$i-$timeslot.dump
+    echo "Backup and Vacuum complete at $timeinfo for time slot $timeslot on database: $i " >> $logfile
+  fi
+done
+find $backup_dir -type f -prune -mtime +$number_of_days -exec rm -f {} \;
+
+############################################################
+# the next step...
+# open your terminal
+ 
+# su
+# su postgres
+# crontab -e
+# 45 */4 * * * /var/lib/postgresql/backup.sh > /dev/null 2>&1
+############################################################
