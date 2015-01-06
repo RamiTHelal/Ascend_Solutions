daf46e6f2861a7382326197395c5ddc47b8bfe93
ramitalat <rami_helal@gmail.com>
2015-01-06 20:55:43 +0200

    Create create_backup_dir.sh

---
+++ Buraq/VM/create_backup_dir.sh
@@ -0,0 +1,16 @@
+############################################################
+# Create the Backup-Script /var/lib/postgresql/backup.sh
+# Modified by Rami Talat 06-JAN-2014
+#
+# ***  assure that the backup_dir is already exist first  ***
+# 
+# Reference: https://www.odoo.com/forum/help-1/question/how-to-setup-a-regular-postgresql-database-backup-4728
+############################################################
+ 
+#!/bin/bash
+
+# Location to place backups.
+backup_dir="/var/backups/postgres_db"
+
+sudo mkdir $backup_dir
+sudo chown postgres $backup_dir
