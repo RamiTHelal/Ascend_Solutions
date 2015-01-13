277db652846c5901fc2b41010f7292d844262f99
ramitalat <rami_helal@gmail.com>
2015-01-14 00:36:55 +0200

    Update create_backup_dir.sh

--- Buraq/VM/create_backup_dir.sh
+++ Buraq/VM/create_backup_dir.sh
@@ -11,17 +11,12 @@ backup_dir="/var/backups/postgres_db"
 sudo mkdir $backup_dir
 sudo chown postgres $backup_dir
 
-# go to the postgres home dir
-cd /var/lib/postgresql
-
 # create the backup script ...
-sudo wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/backup.sh
-sudo chown postgres backup.sh
+sudo su - postgres -c "wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/backup.sh"
 sudo chmod +x backup.sh
 
 # modifying postgres crontab 
-sudo su - postgres -c "crontab -l | { cat; echo "45 */4 * * * /var/lib/postgresql/backup.sh > /dev/null 2>&1"; } | crontab - " 2> /dev/null || true
-exit
+sudo su - postgres -c "crontab -l | { cat; echo \"45 */4 * * * /var/lib/postgresql/backup.sh > /dev/null 2>&1\"; } | crontab -"
 # return back to your current dir
 cd -
 
