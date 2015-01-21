08655c8e691afba6f0b3e11dfbadcbb90128194a
ramitalat <rami_helal@gmail.com>
2015-01-22 01:00:03 +0200

    Create script_ext.sh

---
+++ Buraq/VM/script_ext.sh
@@ -0,0 +1,7 @@
+############################################################
+# Extesion to odoo_install.sh script_ext.sh
+# Created by Rami Talat 22-JAN-2014
+#
+############################################################
+
+sudo apt-get install pgadmin3 -y
