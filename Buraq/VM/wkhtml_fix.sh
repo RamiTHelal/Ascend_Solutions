0337f6b9cc759f88d2d64191540f5e8ebba4f272
ramitalat <rami_helal@gmail.com>
2015-02-18 21:58:26 +0200

    Create wkhtml_fix.sh

---
+++ Buraq/VM/wkhtml_fix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+################################################################################
+# wkhtml_fix.sh
+# Author: Rami Talat
+#-------------------------------------------------------------------------------
+#
+# This script Script for Fixing wkhtmltopdf pkg
+#
+################################################################################
+
+# Remove if exist wkhtmltox-0.12.2.1_linux-trusty-i386.deb 
+sudo rm -f wkhtmltox-0.12.2.1_linux-trusty-i386.deb
+
+sudo wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-i386.deb
+sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb             
+sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
+sudo ln -s  /usr/local/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
+
+sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
+sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
