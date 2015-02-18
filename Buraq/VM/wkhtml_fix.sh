075c2d5dba06aac81a26af44bb8a8cc57f780dc5
ramitalat <rami_helal@gmail.com>
2015-02-18 23:07:15 +0200

    Update wkhtml_fix.sh

--- Buraq/VM/wkhtml_fix.sh
+++ Buraq/VM/wkhtml_fix.sh
@@ -11,6 +11,9 @@
 # Remove if exist wkhtmltox-0.12.2.1_linux-trusty-i386.deb 
 sudo rm -f wkhtmltox-0.12.2.1_linux-trusty-i386.deb
 
+sudo apt-get install -y wkhtmltopdf
+sudo apt-get install -f
+
 sudo wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-i386.deb
 sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb             
 sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
