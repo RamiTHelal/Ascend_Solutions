313857bba2db31ec211b1a645ac81455007ea88c
ramitalat <rami_helal@gmail.com>
2015-03-24 14:39:41 +0200

    Update extra_apps.sh

--- Buraq/VM/extra_apps.sh
+++ Buraq/VM/extra_apps.sh
@@ -15,13 +15,15 @@ sudo su $OE_USER -c "wget -O rami_2.zip https://github.com/codup/odoo-eam/archiv
 
 sudo su $OE_USER -c "wget -O rami_3.zip https://github.com/OCA/account-financial-reporting/archive/8.0.zip"
 
+sudo su $OE_USER -c "wget -O rami_4.zip https://github.com/odoo/odoo/archive/8.0.zip"
+
 # Unzip all
 sudo su $OE_USER -c "find . -name 'rami_*.zip' -print0 | xargs -0 -I {} -P 10 unzip {}"
 
 # Cleanup the zips
 sudo su $OE_USER -c "find . -name 'rami_*.zip' -print0 | xargs -0 -I {} -P 10 rm {}"
 
-# Move teh subdirs to the custom dir
+# Move the subdirs to the custom dir
 sudo su $OE_USER -c "mv account-financial-reporting-8.0/ ramid_account-financial-reporting-8.0/"
 sudo su $OE_USER -c "mv odoo-eam-master/ ramid_odoo-eam-master/"
 sudo su $OE_USER -c "mv dvit-odoo8-master/ ramid_dvit-odoo8-master/"
